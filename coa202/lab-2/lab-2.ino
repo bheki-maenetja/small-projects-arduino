a402b993cbce35f8c56ecbe934236183cd8a9ba4
bheki-maenetja <bhekimaenetja@gmail.com>
2021-02-17 15:17:00 +0000

    working on lab 2 exercises

--- coa202/lab-2/lab-2.ino
+++ coa202/lab-2/lab-2.ino
@@ -1,9 +1,26 @@
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h> 
+#include <utility/Adafruit_MCP23017.h>
+
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  lcd.begin(16,2);
+//  lcd.setCursor(0,0);
+//  lcd.print("I am learning");
+//  lcd.setCursor(0,1);
+//  lcd.print("Wazzup!!!");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+//  String myString = Serial.readString();
+//  lcd.setCursor(0,0);
+//  lcd.print("T: " + myString);
+//  lcd.setCursor(0,1);
+//  lcd.print("B: " + myString);
+//  delay(2000);
+//  lcd.clear();
 }
