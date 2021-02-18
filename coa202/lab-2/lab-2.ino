0bb29645c804eee539768eb8ff375f9829f4b32b
bheki-maenetja <bhekimaenetja@gmail.com>
2021-02-18 11:25:06 +0000

    completed lab 2 tasks

--- coa202/lab-2/lab-2.ino
+++ coa202/lab-2/lab-2.ino
@@ -5,6 +5,13 @@
 #include <GKScroll.h>
 
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
+unsigned long myTime;
+
+byte straightPerson1x0[] = { B00000, B01110, B01110, B00100, B11111, B00100, B01010, B10001 };
+byte rightPerson1x1[] = { B00000, B01110, B01110, B00101, B01110, B10100, B01010, B10001 };
+byte leftPerson1x2[] = { B00000, B01110, B01110, B10100, B01110, B00101, B01010, B10001 };
+byte upPerson1x3[] = { B00000, B01110, B01110, B10101, B01110, B00100, B01010, B10001 };
+byte downPerson1x4[] = { B00000, B01110, B01110, B00100, B01110, B10101, B01010, B10001 };
 
 void setup() {
   // put your setup code here, to run once:
@@ -19,13 +26,50 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-//  String myString = Serial.readString();
-//  lcd.setCursor(0,0);
-//  lcd.print("T: " + myString);
-//  lcd.setCursor(0,1);
-//  String otherString = Serial.readString();
-//  lcd.print("B: " + otherString);
-//  
-  lcd.print(ScrollLine("Wazzup!!!", 5, 16));
-  delay(100);
+  uint8_t buttons = lcd.readButtons();
+
+  if (buttons) {
+    lcd.clear();
+    lcd.setCursor(0,0);
+    if (buttons & BUTTON_UP) {
+      lcd.print("UP ");
+      lcd.setBacklight(5);
+      
+      lcd.createChar(0, upPerson1x3);
+      lcd.setCursor(0, 1);
+      lcd.write(0);
+    }
+    if (buttons & BUTTON_DOWN) {
+      lcd.print("DOWN ");
+      lcd.setBacklight(1);
+
+      lcd.createChar(0, downPerson1x4);
+      lcd.setCursor(0, 1);
+      lcd.write(0);
+    }
+    if (buttons & BUTTON_LEFT) {
+      lcd.print("LEFT ");
+      lcd.setBacklight(2);
+
+      lcd.createChar(0, leftPerson1x2);
+      lcd.setCursor(0, 1);
+      lcd.write(0);
+    }
+    if (buttons & BUTTON_RIGHT) {
+      lcd.print("RIGHT ");
+      lcd.setBacklight(3);
+
+      lcd.createChar(0, rightPerson1x1);
+      lcd.setCursor(0, 1);
+      lcd.write(0);
+    }
+    if (buttons & BUTTON_SELECT) {
+      lcd.print("SELECT ");
+      lcd.setBacklight(4);
+
+      lcd.createChar(0, straightPerson1x0);
+      lcd.setCursor(0, 1);
+      lcd.write(0);
+    }
+  }
 }
