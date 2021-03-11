8b6f9a7d0ec30cb8b0a63ff36ad3a083eee819e1
bheki-maenetja <bhekimaenetja@gmail.com>
2021-03-11 10:27:46 +0000

    lab 4 exercises

--- sandbox/sandbox.ino
+++ sandbox/sandbox.ino
@@ -14,13 +14,57 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
 #define VIOLET 0x5
 #define WHITE 0x7
 
+char msg[] = "locked";
+char *btn_sequence[] = { "0", "0", "0" };
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16,2);
+  lcd.setCursor(0,0);
+  lcd.print(msg);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  static uint8_t old_buttons = lcd.readButtons();
+  uint8_t buttons = lcd.readButtons();
 
+  int button_changes = old_buttons & ~buttons;
+
+  if (button_changes & BUTTON_LEFT) {
+    if (btn_sequence[0] != "L") {
+        btn_sequence[0] = "L";
+    } else {
+        btn_sequence[0] = "0";
+    }
+  }
+  if (button_changes & BUTTON_RIGHT) {
+    if (btn_sequence[0] == "L" && btn_sequence[1] != "R") {
+        btn_sequence[1] = "R";
+    } else {
+        btn_sequence[1] = "0";
+    }
+  }
+
+  if (button_changes & (BUTTON_UP & BUTTON_UP)) {
+    if (btn_sequence[0] == "L" && btn_sequence[1] != "R" && btn_sequence[2] != "UD") {
+        btn_sequence[2] = "UD";
+    } else {
+        btn_sequence[2] = "0";
+    }
+  }
+
+  print_elems();
+  old_buttons = buttons;
+}
+
+void print_elems() {
+  for (int i = 0; i < 3; i++) {
+    Serial.println(btn_sequence[i]);
+  }
+}
+
+void print_message() {
+  lcd.print(msg);
 }
