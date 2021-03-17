70e2545b637bd6b154ea6432eb52cfc9f4e31302
bheki-maenetja <bhekimaenetja@gmail.com>
2021-03-17 15:35:44 +0000

    almost completed lab 4 exercises

--- coa202/lab-4/lab-4.ino
+++ coa202/lab-4/lab-4.ino
@@ -1,3 +1,4 @@
+// Boilerplate code for a new project
 #include <Wire.h>
 #include <Adafruit_RGBLCDShield.h> 
 #include <utility/Adafruit_MCP23017.h>
@@ -13,16 +14,78 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
 #define VIOLET 0x5
 #define WHITE 0x7
 
+//char msg[] = "locked";
+char *btn_sequence[] = { "0", "0", "0" };
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16,2);
-  lcd.setCursor(1,0);
-  lcd.print("locked");
+  lcd.setCursor(0,0);
+  change_message("locked");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  lcd.scrollDisplayLeft();
-  delay(250);
+  static uint8_t old_buttons = lcd.readButtons();
+  uint8_t buttons = lcd.readButtons();
+
+  int button_changes = old_buttons & ~buttons;
+
+  if (button_changes & BUTTON_LEFT) {
+    if (btn_sequence[0] != "L") {
+        btn_sequence[0] = "L";
+    } else {
+        reset_sequence();
+    }
+  }
+  if (button_changes & BUTTON_RIGHT) {
+    if (btn_sequence[0] == "L" && btn_sequence[1] != "R") {
+        btn_sequence[1] = "R";
+    } else {
+        reset_sequence();
+    }
+  }
+
+  if ((button_changes & BUTTON_UP) && (button_changes & BUTTON_DOWN)) {
+    Serial.println("TRUE");
+    if (btn_sequence[0] == "L" && btn_sequence[1] == "R" && btn_sequence[2] != "UD") {
+        btn_sequence[2] = "UD";
+    } else {
+        reset_sequence();
+    }
+  }
+
+  if (old_buttons != buttons) {
+    check_sequence();
+  }
+  print_elems();
+  old_buttons = buttons;
+}
+
+void print_elems() {
+  for (int i = 0; i < 3; i++) {
+    Serial.println(btn_sequence[i]);
+  }
+}
+
+void change_message(char msg[]) {
+  lcd.clear();
+  lcd.print(msg);
+}
+
+void check_sequence() {
+  for (int i = 0; i < 3; i++) {
+    if (btn_sequence[i] == "0") {
+      change_message("locked");
+      return; 
+    }
+  }
+  change_message("unlocked");
+}
+
+void reset_sequence() {
+  btn_sequence[0] = "0";
+  btn_sequence[1] = "0";
+   btn_sequence[2] = "0";
 }
