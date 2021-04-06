ae27f3974b693a5e6a00a67990f59fe631dcdee4
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-06 16:04:06 +0100

    figured out basic menu - need data structure for house

--- coa202/coursework/coursework.ino
+++ coa202/coursework/coursework.ino
@@ -1,9 +1,29 @@
+// Boilerplate code for a new project
+#include <Wire.h>
+#include <Adafruit_RGBLCDShield.h> 
+#include <utility/Adafruit_MCP23017.h>
+#include <string.h>
+
+Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
+
+#define RED 0x1
+#define YELLOW 0x3
+#define GREEN 0x2
+#define TEAL 0x6
+#define BLUE 0x4
+#define VIOLET 0x5
+#define WHITE 0x7
+
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  lcd.begin(16,2);
+  Serial.println("BASIC");
+  lcd.setCursor(0,0);
+  lcd.print("SELECT:");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
