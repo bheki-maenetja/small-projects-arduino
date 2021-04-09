5c7aec1d750c3a4886987cdde34228747f2e7c97
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-09 14:39:22 +0100

    need to sort out data structure for coursework

--- sandbox/sandbox.ino
+++ sandbox/sandbox.ino
@@ -14,13 +14,50 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
 #define VIOLET 0x5
 #define WHITE 0x7
 
+typedef struct action {
+  String action_name;
+  int level;
+  String on_time;
+  String off_time;
+};
+
+typedef struct type {
+  String typeName;
+  action actions[2];
+};
+
+typedef struct room {
+  String room_name;
+  type types[2]; 
+};
+
+typedef struct house_floor {
+  String floor_name;
+  room rooms[3];
+};
+
+typedef struct house {
+  house_floor floors[2];
+};
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16, 2);
+  house myHouse;
+  setUpHouse(&myHouse);
+  Serial.println(myHouse.floors[0].floor_name);
+  Serial.println(myHouse.floors[1].floor_name);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
 
+void setUpHouse(house *houseObj) {
+  (*houseObj).floors[0].floor_name = "Ground";
+  (*houseObj).floors[1].floor_name = "First";
+  (*houseObj).floors[0].rooms[0].room_name = "kitchen";
+  (*houseObj).floors[0].rooms[1].room_name = "hall";
+  (*houseObj).floors[0].rooms[2].room_name = "living room";
 }
