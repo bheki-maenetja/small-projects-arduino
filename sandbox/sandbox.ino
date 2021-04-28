5b42e7ea13e9df717f8cd79a900e16f6e65ef4cd
bheki-maenetja <bhekimaenetja@gmail.com>
2021-04-28 15:41:32 +0100

    effectively done with coding part of coa202 coursewor

--- sandbox/sandbox.ino
+++ sandbox/sandbox.ino
@@ -14,13 +14,33 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield ();
 #define VIOLET 0x5
 #define WHITE 0x7
 
+#ifdef __arm__
+extern "C" char* sbrk(int incr); 
+#else // __ARM__
+extern char *__brkval;
+#endif // __arm__
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   lcd.begin(16,2);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.print(freeMemory());
+  Serial.print(" kb\n");
+  delay(1000);
+}
 
+int freeMemory() {
+  char top;
+#ifdef __arm__
+  return &top - reinterpret_cast<char*>(sbrk(0));
+#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+  return &top - __brkval;
+#else  // __arm__
+  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+#endif  // __arm__
 }
